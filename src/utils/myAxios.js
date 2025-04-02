ae974816b48bb9fbdb2eec2bad5b86455f6014b3
hcl_xbg <3011002463@qq.com>
2025-04-02 20:50:24 +0800

    patch

--- src/utils/myAxios.js
+++ src/utils/myAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const myAxios = axios.create({
-  baseURL:'http://localhost:8080',
+  baseURL:process.env.NODE_ENV==='development'?'http://localhost:8080':'https://github.com/B-612-prince/',
   timeout:10000,
   withCredentials:true,
 });
